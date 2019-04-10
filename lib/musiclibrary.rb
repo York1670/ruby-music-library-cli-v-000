0637bdd0c3f8115fac6a6e0acbea81be156f224d
Bobbi York <york1670@gmail.com>
2019-04-10 06:01:46 +0000

    Automatically backed up by Learn

--- lib/musiclibrary.rb
+++ lib/musiclibrary.rb
@@ -24,7 +24,8 @@ class MusicLibraryController
       puts "To quit, type 'exit'."
       puts "What would you like to do?"
       input = gets.strip
-      
+    end
+
     case input
       when "list songs"
         list_songs
